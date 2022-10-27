01c9c6493d09be760be66509076c6ec7f194af0c
Kent Martin Åsvang <kent@asvang.no>
2022-10-27 23:12:08 +0200

    finished schedules-method

--- pysit/src/main.py
+++ pysit/src/main.py
@@ -34,68 +34,94 @@ class Trip:
     service_id: str
 
 
-def load_trips():
-    lines = ""
-    with open(FILES["TRIPS"], "r") as trips:
-        lines = trips.readlines()
-
-    header = lines[0].split(",")
-    print(header)
-    assert header[0] == "route_id"
-    assert header[1] == "service_id"
-    assert header[2] == "trip_id"
+class GTFS:
 
     trips = []
     trips_ix_by_route = {}
+    stop_times = []
+    stop_times_ix_by_trip = {}
 
-    i = 0
-    for line in lines[1:]:
-        cells = line.split(",")
-        route_id = cells[0]
-        trips.append(Trip(cells[2], route_id, cells[1]))
+    def __init__(self):
+        self.trips, self.trips_ix_by_route = self._load_trips()
+        self.stop_times, self.stop_times_ix_by_trip = self._load_stop_times()
 
-        if route_id in trips_ix_by_route:
-            trips_ix_by_route[route_id].append(i)
-        else:
-            trips_ix_by_route[route_id] = [i]
+    def schedule_for_route(self, route):
+        trips = []
+        trip_ixs = self.trips_ix_by_route.get(route)
+        if (trip_ixs != None):
+            for trip_ix in trip_ixs:
+                trip = self.trips[trip_ix]
+                stop_time_ixs = self.stop_times_ix_by_trip[trip[0]]
+                schedules = []
+                for stop_time_ix in stop_time_ixs:
+                    stop_time = self.stop_times[stop_time_ix]
+                    schedules.append((stop_time[3], stop_time[1], stop_time[2]))
 
-        i += 1
+                trips.append((trip[2], trip[0], trip[1], schedules))
 
-    return (trips, trips_ix_by_route)
+        return trips
 
 
-def load_stop_times():
-    start_time = time.time()
-    lines = ""
-    with open(FILES["STOP_FILES"], "r") as stop_file:
-        lines = stop_file.readlines()
+    def _load_trips(self):
+        lines = ""
+        with open(FILES["TRIPS"], "r") as trips:
+            lines = trips.readlines()
+
         header = lines[0].split(",")
-        assert header[0] == "trip_id"
-        assert header[1] == "arrival_time"
-        assert header[2] == "departure_time"
-        assert header[3] == "stop_id"
+        print(header)
+        assert header[0] == "route_id"
+        assert header[1] == "service_id"
+        assert header[2] == "trip_id"
 
-        stop_times = []
-        stop_times_ix_by_trips = {}
+        trips = []
+        trips_ix_by_route = {}
 
         i = 0
         for line in lines[1:]:
             cells = line.split(",")
-            trip_id = cells[0]
+            route_id = cells[0]
+            # trip_id, route_id, service_id
+            trips.append(Trip(cells[2], route_id, cells[1]))
 
-            if trip_id in stop_times_ix_by_trips:
-                stop_times_ix_by_trips[trip_id].append(i)
+            if route_id in trips_ix_by_route:
+                trips_ix_by_route[route_id].append(i)
             else:
-                stop_times_ix_by_trips[trip_id] = [i]
+                trips_ix_by_route[route_id] = [i]
 
             i += 1
-        
+
+        return (trips, trips_ix_by_route)
+
+
+    def _load_stop_times(self):
+        start_time = time.time()
+        lines = ""
+        with open(FILES["STOP_FILES"], "r") as stop_file:
+            lines = stop_file.readlines()
+            header = lines[0].split(",")
+            assert header[0] == "trip_id"
+            assert header[1] == "arrival_time"
+            assert header[2] == "departure_time"
+            assert header[3] == "stop_id"
+
+            stop_times = []
+            stop_times_ix_by_trips = {}
+
+            i = 0
+            for line in lines[1:]:
+                cells = line.split(",")
+                trip_id = cells[0]
+
+                if trip_id in stop_times_ix_by_trips:
+                    stop_times_ix_by_trips[trip_id].append(i)
+                else:
+                    stop_times_ix_by_trips[trip_id] = [i]
+
+                i += 1
+            
         end_time = time.time()
         print(f"Elapsed time in seconds: {end_time-start_time}")
         return (stop_times, stop_times_ix_by_trips)
-
-
-    pass
 
 
 if __name__ == "__main__":
